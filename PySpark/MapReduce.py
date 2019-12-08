8ecd66290e6f41fece398178404442277cbdbaf7
jdubinsk <jan.michal.dubinski@cern.ch>
2019-12-08 17:33:24 +0100

    started PySpark

--- PySpark/MapReduce.py
+++ PySpark/MapReduce.py
@@ -1,4 +1,5 @@
 from pyspark import SparkContext, SparkConf
+
 conf = SparkConf().setMaster("local[2]").setAppName("Spark Count")
 spark = SparkContext(conf=conf)
 file_names = spark.textFile("hdfs://...")
@@ -7,8 +8,7 @@ file_rel = spark.textFile("hdfs//...")
 file_names = file_names.flatMap(lambda line: line.split(","))
 file_rel = file_rel.flatMap(lambda line: line.split(","))
 
-context = file_names.map(lambda id: (id.split(",")[0], "name" + "   " + id.split(",")[1]))
-
-context = file_names.map(lambda id: (id.split(",")[0], "rel" + "   " + id.split(",")[1]))
-
-context=context.reduceByKey(lambda a, b: a + b)
+rdd_names = file_names.map(lambda id_: (id_.split(",")[0], "name" + "   " + id_.split(",")[1]))
+rdd_rel = file_names.map(lambda id_: (id_.split(",")[0], "rel" + "   " + id_.split(",")[1]))
+rdd = rdd_names.join(rdd_rel)
+context = rdd.reduceByKey(lambda a, b: a + b)
