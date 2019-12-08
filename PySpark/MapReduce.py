ee2b2a0363b00d7836b44ee1856b2bc01d9ecb5e
jdubinsk <jan.michal.dubinski@cern.ch>
2019-12-08 17:39:12 +0100

    PyStark first try :D

--- PySpark/MapReduce.py
+++ PySpark/MapReduce.py
@@ -1,9 +1,10 @@
 from pyspark import SparkContext, SparkConf
+import sys
 
 conf = SparkConf().setMaster("local[2]").setAppName("Spark Count")
 spark = SparkContext(conf=conf)
-file_names = spark.textFile("hdfs://names.txt")
-file_rel = spark.textFile("hdfs//relations.txt")
+file_names = spark.textFile("hdfs://" + sys.argv[0])
+file_rel = spark.textFile("hdfs//" + sys.argv[1])
 
 file_names = file_names.flatMap(lambda line: line.split(","))
 file_rel = file_rel.flatMap(lambda line: line.split(","))
@@ -12,4 +13,4 @@ rdd_names = file_names.map(lambda id_: (id_.split(",")[0], "name" + "   " + id_.
 rdd_rel = file_names.map(lambda id_: (id_.split(",")[0], "rel" + "   " + id_.split(",")[1]))
 rdd = rdd_names.join(rdd_rel)
 context = rdd.reduceByKey(lambda a, b: a + b)
-context.saveAstTextFile("hdfs//output_pyspark.txt")
+context.saveAstTextFile("hdfs//" + sys.args[2])
