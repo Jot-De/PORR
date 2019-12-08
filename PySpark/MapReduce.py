9d2e13b4fcfde19193a6dff63c9e70d406366990
jdubinsk <jan.michal.dubinski@cern.ch>
2019-12-09 00:02:13 +0100

    countbykey

--- PySpark/MapReduce.py
+++ PySpark/MapReduce.py
@@ -1,6 +1,6 @@
 from pyspark import SparkContext, SparkConf
 
-conf = SparkConf().setMaster("local[2]").setAppName("Spark Count")
+conf = SparkConf().setMaster("local").setAppName("Spark Count")
 spark = SparkContext(conf=conf)
 file_names = spark.textFile("hdfs:///friends/names_short.csv")
 file_rel = spark.textFile("hdfs:///friends/relations_short.csv")
@@ -10,6 +10,7 @@ file_names = file_names.map(lambda line: line.split(","))
 file_rel = file_rel.map(lambda line: line.split(","))
 
 rdd_names = file_names.map(lambda id_: (id_[0], "name" + "   " + id_[1]))
-rdd_rel = file_rel.map(lambda id_: (id_[0], "rel" + "   " + id_[1])).countByKey()
+rdd_rel = file_rel.map(lambda id_: (id_[0], 1)).reduceByKey(lambda x, y: x + y)
+
 rdd = rdd_names.join(rdd_rel)
 rdd.saveAsTextFile("hdfs///friends/output/spark")
