95dec1055cca2147189d62b08a97c1f81bf23b49
jdubinsk <jan.michal.dubinski@cern.ch>
2019-12-08 21:58:10 +0100

    quick fix XD

--- PySpark/MapReduce.py
+++ PySpark/MapReduce.py
@@ -2,8 +2,8 @@ from pyspark import SparkContext, SparkConf
 
 conf = SparkConf().setMaster("local[2]").setAppName("Spark Count")
 spark = SparkContext(conf=conf)
-file_names = spark.textFile("hdfs://friends/names.txt")
-file_rel = spark.textFile("hdfs//friends/relations.txt")
+file_names = spark.textFile("hdfs:///friends/names.txt")
+file_rel = spark.textFile("hdfs///friends/relations.txt")
 
 file_names = file_names.flatMap(lambda line: line.split(","))
 file_rel = file_rel.flatMap(lambda line: line.split(","))
@@ -12,4 +12,4 @@ rdd_names = file_names.map(lambda id_: (id_.split(",")[0], "name" + "   " + id_.
 rdd_rel = file_names.map(lambda id_: (id_.split(",")[0], "rel" + "   " + id_.split(",")[1]))
 rdd = rdd_names.join(rdd_rel)
 context = rdd.reduceByKey(lambda a, b: a + b)
-context.saveAstTextFile("hdfs//friends/output/spark.txt")
+context.saveAstTextFile("hdfs///friends/output/spark.txt")
