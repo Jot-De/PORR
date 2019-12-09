dd5a25f519538c788bbf4e6fbc8ab47080cd5c32
LAPTOP-R60VRCK7\joann <joannakaletaa@gmail.com>
2019-12-09 01:23:28 +0100

    MapReduce fixed

--- PySpark/MapReduce.py
+++ PySpark/MapReduce.py
@@ -1,16 +1,12 @@
 from pyspark import SparkContext, SparkConf
 
-conf = SparkConf().setMaster("local[2]").setAppName("Spark Count")
+conf = SparkConf().setMaster("local").setAppName("Spark Count")
 spark = SparkContext(conf=conf)
+
 file_names = spark.textFile("hdfs:///friends/names_short.csv")
-file_rel = spark.textFile("hdfs///friends/relations_short.csv")
+file_rel = spark.textFile("hdfs:///friends/relations_short.csv")
 
-
-file_names = file_names.flatMap(lambda line: line.split(","))
-file_rel = file_rel.flatMap(lambda line: line.split(","))
-
-rdd_names = file_names.map(lambda id_: (id_.split(",")[0], "name" + "   " + id_))
-rdd_rel = file_rel.map(lambda id_: (id_.split(",")[0], "rel" + "   " + id_))
-rdd = rdd_names.join(rdd_rel)
-context = rdd.reduceByKey(lambda a, b: a + b)
-context.saveAsTextFile("hdfs///friends/output/spark.txt")
+rdd_names = file_names.map(lambda line: line.split(","))
+rdd_rel = file_rel.flatMap(lambda line: line.split(",")).map(lambda id_: (id_, 1)).reduceByKey(lambda x, y: x + y)
+rdd = rdd_names.join(rdd_rel).map(lambda x: x[1])
+rdd.saveAsTextFile("hdfs:///friends/output/spark")
