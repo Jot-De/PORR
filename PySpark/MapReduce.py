3721829c8992fceb747c95efaf9a0abe5ce5ddae
LAPTOP-R60VRCK7\joann <joannakaletaa@gmail.com>
2019-12-12 23:43:51 +0100

    add sys.argv

--- PySpark/MapReduce.py
+++ PySpark/MapReduce.py
@@ -1,12 +1,22 @@
+import sys
+import shutil
 from pyspark import SparkContext, SparkConf
 
 conf = SparkConf().setMaster("local").setAppName("Spark Count")
 spark = SparkContext(conf=conf)
 
-file_names = spark.textFile("hdfs:///friends/names_short.csv")
-file_rel = spark.textFile("hdfs:///friends/relations_short.csv")
+#example names "hdfs:///friends/names_short.csv"
+file_names = spark.textFile(sys.argv[1])
+#example relations: "hdfs:///friends/relations_short.csv"
+file_rel = spark.textFile(sys.argv[2])
 
 rdd_names = file_names.map(lambda line: line.split(","))
 rdd_rel = file_rel.flatMap(lambda line: line.split(",")).map(lambda id_: (id_, 1)).reduceByKey(lambda x, y: x + y)
 rdd = rdd_names.join(rdd_rel).map(lambda x: x[1])
-rdd.saveAsTextFile("hdfs:///friends/output/spark")
+
+#example output: "hdfs:///friends/output/spark"
+try:
+    shutil.rmtree(sys.argv[3], ignore_errors=True)
+except:
+    pass
+rdd.saveAsTextFile(sys.argv[3])
