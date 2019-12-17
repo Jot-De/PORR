8032e67ca5ac0fa9e72c16e3397671a578e8a4ce
LAPTOP-R60VRCK7\joann <joannakaletaa@gmail.com>
2019-12-17 14:12:49 +0100

    delete local master

--- PySpark/MapReduce.py
+++ PySpark/MapReduce.py
@@ -1,8 +1,7 @@
 import sys
-import shutil
 from pyspark import SparkContext, SparkConf
 
-conf = SparkConf().setMaster("local").setAppName("Spark Count")
+conf = SparkConf().setAppName("Spark Count")
 spark = SparkContext(conf=conf)
 
 #example names "hdfs:///friends/names_short.csv"
@@ -15,8 +14,4 @@ rdd_rel = file_rel.flatMap(lambda line: line.split(",")).map(lambda id_: (id_, 1
 rdd = rdd_names.join(rdd_rel).map(lambda x: x[1])
 
 #example output: "hdfs:///friends/output/spark"
-try:
-    shutil.rmtree(sys.argv[3], ignore_errors=True)
-except:
-    pass
 rdd.saveAsTextFile(sys.argv[3])
